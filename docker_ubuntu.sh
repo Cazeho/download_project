dd55205c3b66c4fee293b1b0484e6c7a3d1a11ab
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-05-09 10:08:55 +0200

    Create docker_ubuntu.sh

---
+++ docker_ubuntu.sh
@@ -0,0 +1,14 @@
+#docker (ubuntu)
+
+apt update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+#docker-compose
+
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
