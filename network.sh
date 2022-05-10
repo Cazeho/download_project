f50c181b477107bc5788d62743bce105f698b6c9
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-05-10 14:43:43 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,5 +1,6 @@
 # Access GITALAB with VPN
 
-sudo echo "10.0.45.54 gitlab.altrnativ.xyz" | tee -a /etc/hosts
+sudo echo "search gitlab.altrnativ.xyz \n nameserver 10.0.45.54" | tee -a /etc/resolv.conf
+#sudo echo "10.0.45.54 gitlab.altrnativ.xyz" | tee -a /etc/hosts
 sudo systemctl daemon-reload
 sudo /bin/systemctl restart systemd-hostnamed
