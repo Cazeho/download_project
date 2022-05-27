2cec5d8d823715fe3e0f57c00cfc50de58b8c59d
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-05-27 11:45:48 +0200

    Create pam_go.sh

---
+++ pam_go.sh
@@ -0,0 +1,9 @@
+cd /opt
+wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
+tar xvf go1.18.2.linux-amd64.tar.gz
+PATH=$PATH:/opt/go/bin
+export PATH
+GOROOT=/opt/go
+export GOROOT
+GOPATH=/root
+export GOPATH
