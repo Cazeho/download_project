0f821728995ebc9f76ec524abd072b4eed1f49c3
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-05-27 15:07:40 +0200

    Update pam_go.sh

--- pam_go.sh
+++ pam_go.sh
@@ -1,4 +1,6 @@
 cd /opt 
 curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/go/bin | tee ~/.profile
+source ~/.profile
+go version
