d5c2a78817cc6e904eefa3e90edb9482eeaa6a6b
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-06-01 14:37:07 +0200

    Update docker_ubuntu.sh

--- docker_ubuntu.sh
+++ docker_ubuntu.sh
@@ -1,5 +1,6 @@
 #docker (ubuntu)
 
+apt install jq -y
 apt update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -9,6 +10,7 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 
 #docker-compose
+$version=curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"
 
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/$version/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
