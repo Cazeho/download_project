237a5019b57092f66b609795b6df56b5c781acfb
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-06-01 14:42:36 +0200

    Update docker_ubuntu.sh

--- docker_ubuntu.sh
+++ docker_ubuntu.sh
@@ -10,7 +10,8 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 
 #docker-compose
-$version=curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+
 
 curl -SL https://github.com/docker/compose/releases/download/$(version)/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
