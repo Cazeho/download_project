7a6b307f55ac438a9f5a49a87782b6ab408531c2
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-07-01 10:30:44 +0200

    Update docker_debian.sh

--- docker_debian.sh
+++ docker_debian.sh
@@ -6,6 +6,7 @@ apt-get install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 
