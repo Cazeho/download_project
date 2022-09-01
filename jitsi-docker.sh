addf41104931c4dac5ef80fc3b1961c498d7edfe
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-09-01 16:40:21 +0200

    Update jitsi-docker.sh

--- jitsi-docker.sh
+++ jitsi-docker.sh
@@ -2,4 +2,4 @@
 
 #get latest version of the stable release
 
-version=curl -s https://api.github.com/repos/jitsi/docker-jitsi-meet/releases/latest | jq -r ".tag_name"
+version=$(curl -s https://api.github.com/repos/jitsi/docker-jitsi-meet/releases/latest | jq -r ".tag_name")
