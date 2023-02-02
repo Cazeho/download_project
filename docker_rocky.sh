7cf6cb9fb8e86654b33b1b5789d24ec78611690e
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-02-02 14:37:48 +0100

    Update docker_rocky.sh

--- docker_rocky.sh
+++ docker_rocky.sh
@@ -34,7 +34,7 @@ docker_install() {
 
 
 docker_compose_install() {
-     dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin > /dev/null
+     dnf -y install jq docker-ce docker-ce-cli containerd.io docker-compose-plugin > /dev/null
      version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
      curl -SL https://github.com/docker/compose/releases/download/$version/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose &> /dev/null
      chmod +x /usr/local/bin/docker-compose > /dev/null
