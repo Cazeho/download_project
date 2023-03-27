11130b9e6fb493c7a9491e0e6a2f3618ba197071
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-03-27 03:58:42 +0200

    Update cyberscreen.ps1

--- cyberscreen.ps1
+++ cyberscreen.ps1
@@ -1,3 +1,18 @@
+$url = "https://media.cdn.teamtailor.com/images/s3/teamtailor-production/user_picture_1200-v5/image_uploads/1bca5182-3f71-490e-b17d-a314e947ed1d/original.png"
+$outputDir = "C:\Users\vagrant\Pictures"
+$outputPath = Join-Path $outputDir "original.png"
+
+# Download the image using wget
+wget $url -OutFile $outputPath
+
+# Verify that the file was downloaded
+if (Test-Path $outputPath) {
+    Write-Host "Image downloaded successfully to: $outputPath"
+} else {
+    Write-Host "Failed to download image"
+}
+
+
 $user = $env:USERNAME
 
 $ImagePath = "C:\Users\$user\Pictures\original.png"
