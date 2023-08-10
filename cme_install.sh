9e68e4ae69b791a8135b8bfcbe94fc80d8aa7242
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-08-10 14:59:53 +0200

    Update cme_install.sh

--- cme_install.sh
+++ cme_install.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+apt update -y
+apt-get install -y libssl-dev libffi-dev python-dev-is-python3 build-essential python3-pip unzip
+
+wget https://github.com/mpgn/CrackMapExec/archive/refs/tags/v6.0.1.zip
+unzip v6.0.1.zip
+cd CrackMapExec-6.0.1
+pip install poetry
+poetry install
