1aea7a100730ea63c9b0c1c90350fdd049b9f959
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-08-10 15:11:36 +0200

    Update cme_install.sh

--- cme_install.sh
+++ cme_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# debian --> ok
+
 apt update -y
-apt-get install -y libssl-dev libffi-dev python-dev-is-python3 build-essential python3-pip unzip
+apt-get install -y libssl-dev libffi-dev python-dev-is-python3 build-essential python3-pip unzip python3-poetry
 
 wget https://github.com/mpgn/CrackMapExec/archive/refs/tags/v6.0.1.zip
 unzip v6.0.1.zip
 cd CrackMapExec-6.0.1
-pip install poetry
 poetry install
+poetry run crackmapexec
